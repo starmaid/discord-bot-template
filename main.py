05766e6b1a5bb0c3c96b7c62a45c55e77445f228
starmaid <nicholas.masso.14@gmail.com>
2021-01-01 23:35:38 -0500

    fixed global variable usage

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ import discord
 import asyncio
 from discord.ext import commands
 
+# this is the command to use with yout bot. make sure its unique!
+cmd_prefix = '.'
+
+# user to easily quit the bot, or do other restricted commands. if you dont change this, ill be able to do it!!!!
+superuser = 'starmaid#6925'
+
 class Bot(commands.Bot):
     activity = 'a game'
     logoff_msg = '`logging off`'
-    cmd_prefix = '.'
+    cmd_prefix = cmd_prefix
 
 
     def __init__(self):
         # This is the stuff that gets run at startup
-        super().__init__(command_prefix=self.cmd_prefix, self_bot=False, activity=discord.Game(self.activity))
+        super().__init__(command_prefix=cmd_prefix, self_bot=False, activity=discord.Game(self.activity))
         self.remove_command('help')
         self.add_command(self.help)
         self.add_command(self.quit)
@@ -43,11 +49,11 @@ class Bot(commands.Bot):
     @commands.command(pass_context=True)
     async def help(ctx):
         #this is the help command.
-        help_msg = '```<.> CUSTOM BOT <.>\n' + \
-            '\nusage:          command [params]*' + \
+        help_msg = '```<' + cmd_prefix + '> CUSTOM BOT <' + cmd_prefix + '>\n' + \
+            '\nusage:          ' + cmd_prefix + 'command [params]*' + \
             '\n --- availible commands ---' + \
-            '\nhelp                shows this message' + \
-            '\nquit                shuts down the bot (only works for the superuser)' + \
+            '\n' + cmd_prefix + 'help                shows this message' + \
+            '\n' + cmd_prefix + 'quit                shuts down the bot (only works for the superuser)' + \
             '```'
         await ctx.send(help_msg)
         return
@@ -56,7 +62,7 @@ class Bot(commands.Bot):
     @commands.command(pass_context=True)
     async def quit(ctx):
         # quits the bot. helps with testing.
-        if str(ctx.message.author) == 'YOUR USERNAME HERE':
+        if str(ctx.message.author) == superuser:
             await ctx.send(ctx.bot.logoff_msg)
             await ctx.bot.close()
         else:
